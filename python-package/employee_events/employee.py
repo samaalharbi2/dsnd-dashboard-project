5219870ffac0762e874871829b32e74d76fc6d97
Sama Alharbi <176575888+samaalharbi2@users.noreply.github.com>
2025-05-17 17:00:15 +0300

    Update employee.py

--- python-package/employee_events/employee.py
+++ python-package/employee_events/employee.py
@@ -1,65 +1,52 @@
+import pandas as pd
+
 # Import the QueryBase class
-#### YOUR CODE HERE
+from .query_base import QueryBase
 
-# Import dependencies needed for sql execution
-# from the `sql_execution` module
-#### YOUR CODE HERE
-
-# Define a subclass of QueryBase
-# called Employee
-#### YOUR CODE HERE
-
-    # Set the class attribute `name`
-    # to the string "employee"
-    #### YOUR CODE HERE
+# Import the SQL execution mixin 
+from .sql_execution import execute_query 
 
 
-    # Define a method called `names`
-    # that receives no arguments
-    # This method should return a list of tuples
-    # from an sql execution
-    #### YOUR CODE HERE
-        
-        # Query 3
-        # Write an SQL query
-        # that selects two columns 
-        # 1. The employee's full name
-        # 2. The employee's id
-        # This query should return the data
-        # for all employees in the database
-        #### YOUR CODE HERE
-    
+# Define a subclass of QueryBase called Employee
+class Employee(QueryBase):
+    # Set the class attribute `name` to "employee"
+    name = "employee"
 
-    # Define a method called `username`
-    # that receives an `id` argument
-    # This method should return a list of tuples
-    # from an sql execution
-    #### YOUR CODE HERE
-        
-        # Query 4
-        # Write an SQL query
-        # that selects an employees full name
-        # Use f-string formatting and a WHERE filter
-        # to only return the full name of the employee
-        # with an id equal to the id argument
-        #### YOUR CODE HERE
+    def __init__(self, db_path=None):
+        """
+        Initialize the database path and allow override from argument.
+        """
+        super().__init__(db_path=db_path)
 
+    # Define a method called `names` that receives no arguments
+    def names(self):
+        # Query 3: Select full name and employee id for all employees
+        query = """
+            SELECT first_name || ' ' || last_name AS full_name,
+                   employee_id
+            FROM employee
+        """
+        return self.query(query)
 
-    # Below is method with an SQL query
-    # This SQL query generates the data needed for
-    # the machine learning model.
-    # Without editing the query, alter this method
-    # so when it is called, a pandas dataframe
-    # is returns containing the execution of
-    # the sql query
-    #### YOUR CODE HERE
+    # Define a method called `username` that receives an `id` argument
+    def username(self, id):
+        # Query 4: Select full name using WHERE filter
+        query = f"""
+            SELECT first_name || ' ' || last_name AS full_name
+            FROM employee
+            WHERE employee_id = ?
+        """
+        return self.query(query, (id,))
+
+    # Method that returns model data in a pandas DataFrame
     def model_data(self, id):
-
-        return f"""
-                    SELECT SUM(positive_events) positive_events
-                         , SUM(negative_events) negative_events
-                    FROM {self.name}
-                    JOIN employee_events
-                        USING({self.name}_id)
-                    WHERE {self.name}.{self.name}_id = {id}
-                """
+        query = f"""
+            SELECT SUM(positive_events) AS positive_events,
+                   SUM(negative_events) AS negative_events
+            FROM {self.name}
+            JOIN employee_events
+            USING({self.name}_id)
+            WHERE {self.name}.{self.name}_id = ?
+        """
+        result = self.query(query, (id,))
+        return pd.DataFrame(result, columns=["positive_events", "negative_events"])
