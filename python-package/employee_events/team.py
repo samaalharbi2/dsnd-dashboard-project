bf58c19b9fd998f98b0537b53987278a0d6f5817
Sama Alharbi <176575888+samaalharbi2@users.noreply.github.com>
2025-05-17 17:07:56 +0300

    Update team.py

--- python-package/employee_events/team.py
+++ python-package/employee_events/team.py
@@ -1,66 +1,71 @@
 # Import the QueryBase class
-# YOUR CODE HERE
+from .query_base import QueryBase  # As specified in the assignment
 
-# Import dependencies for sql execution
-#### YOUR CODE HERE
+# Import pandas since it’s used below
+import pandas as pd
+
 
 # Create a subclass of QueryBase
-# called  `Team`
-#### YOUR CODE HERE
+# called `Team`
+class Team(QueryBase):
+    # Uses inheritance to add query-related methods from QueryBase/Mixin
 
     # Set the class attribute `name`
     # to the string "team"
-    #### YOUR CODE HERE
+    name = "Team"
 
+    # I added an __init__ method to set the database path
+    def __init__(self, db_path=None):
+        """
+        Initialize the database path, allowing an override.
+        QueryBase.__init__ will default if none given.
+        """
+        super().__init__(db_path=db_path)
 
     # Define a `names` method
     # that receives no arguments
     # This method should return
-    # a list of tuples from an sql execution
-    #### YOUR CODE HERE
-        
-        # Query 5
-        # Write an SQL query that selects
-        # the team_name and team_id columns
-        # from the team table for all teams
-        # in the database
-        #### YOUR CODE HERE
-    
+    # a list of tuples from an SQL execution
+    def names(self):
+        query = """
+            SELECT team_name, team_id
+            FROM team
+        """
+        return self.query(query)
 
     # Define a `username` method
     # that receives an ID argument
     # This method should return
-    # a list of tuples from an sql execution
-    #### YOUR CODE HERE
+    # a list of tuples from an SQL execution
+    def username(self, id):
+        query = """
+            SELECT team_name
+            FROM team
+            WHERE team_id = ?
+        """
+        return self.query(query, (id,))
 
-        # Query 6
-        # Write an SQL query
-        # that selects the team_name column
-        # Use f-string formatting and a WHERE filter
-        # to only return the team name related to
-        # the ID argument
-        #### YOUR CODE HERE
-
-
-    # Below is method with an SQL query
+    # Below is a method with an SQL query
     # This SQL query generates the data needed for
     # the machine learning model.
     # Without editing the query, alter this method
-    # so when it is called, a pandas dataframe
-    # is returns containing the execution of
-    # the sql query
-    #### YOUR CODE HERE
+    # so when it is called, a Pandas DataFrame
+    # is returned containing the execution of
+    # the SQL query
     def model_data(self, id):
-
-        return f"""
-            SELECT positive_events, negative_events FROM (
-                    SELECT employee_id
-                         , SUM(positive_events) positive_events
-                         , SUM(negative_events) negative_events
-                    FROM {self.name}
-                    JOIN employee_events
-                        USING({self.name}_id)
-                    WHERE {self.name}.{self.name}_id = {id}
-                    GROUP BY employee_id
-                   )
-                """
+        # SQL query string that will:
+        # Join the 'team' and 'employee_events' tables on 'team_id'
+        # Filter for a specific team_id (provided as parameter '?')
+        # Group results by employee_id to aggregate event counts
+        # Calculate SUM of positive_events and negative_events per employee
+        query = """
+            SELECT
+                employee_id,
+                SUM(positive_events)   AS positive_events,
+                SUM(negative_events)   AS negative_events
+            FROM employee_events
+            WHERE team_id = ?
+            GROUP BY employee_id
+        """
+        # Return the query results as a cleaned pandas DataFrame
+        return self.pandas_query(query, (id,))
